cbc5fc049bc3d5e6afbb0f44382d3b52fdd53b7e
andreFelipeHFuck <neoandfufe42@gmail.com>
2025-06-16 08:37:56 -0300

    Write bin file

--- lista_exercicios_5/src/main.rs
+++ lista_exercicios_5/src/main.rs
@@ -32,27 +32,29 @@ fn frequency(file: String, map_freq: &mut HashMap<char, u32>) -> io::Result<usiz
     Ok(num)
 }
 
-fn compression_map(file: &String) -> Result<(HashMap<char, u32>, HashMap<char, String>), ()> {
+fn compression_map(file: &String) -> Result<(u32, HashMap<char, u32>, HashMap<char, String>), ()> {
     let mut map: HashMap<char, u32> = HashMap::new();
+    let n: u32;
 
     match frequency(file.to_string(), &mut map) {
             Ok(t) => {
+                n = t as u32;
                 println!("O arquivo possui {t} caracteres.");
             },
-            Err(_) => {println!("Arquivo {file} não existe.");}
+            Err(_) => {panic!("Arquivo {file} não existe.");}
     }
 
     let mut vector_node = VectorNode::VectorNode::new();
     vector_node.build_vector(&mut map);
     let node: Huffman::Huffman =  vector_node.build_tree();
 
-    Ok((map, VectorNode::VectorNode::build_code(node)))
+    Ok((n, map, VectorNode::VectorNode::build_code(node)))
 }
 
-fn compression(file: String) -> io::Result<()> {
+fn compression(file: String, file_bin: String) -> io::Result<()> {
     let mut str_code: String = String::new();
 
-    let (map_freq, map_code) = compression_map(&file).unwrap();
+    let (n, map_freq, map_code) = compression_map(&file).unwrap();
     
     let fs = File::open(file)?;
     let mut buf = Reader::new(&fs);
@@ -66,15 +68,20 @@ fn compression(file: String) -> io::Result<()> {
         str_code += map_code.get(&c).unwrap();
     }
 
-    println!("MAP FREQ: {:?}", map_freq);
-    println!("MAP FREQ: {:?}", map_code);
-    println!("STR CODE: {}", str_code);
-
     let vec_bytes = divide_string(&str_code, 7);
 
-    
+    write(file_bin, n, &map_freq, vec_bytes)?;
 
     Ok(())
+}
+
+fn complete_string(s: &mut String){
+    let size = s.len();
+    if size < 8{
+        for _ in 0..(8 - size){
+            s.push('0');
+        }
+    }
 }
 
 fn divide_string(s: &String, size: usize) -> Vec<String> {
@@ -96,17 +103,25 @@ fn divide_string(s: &String, size: usize) -> Vec<String> {
     }
 
     if !(aux_s.len() == 0) {
+        complete_string(&mut aux_s);
         v.push(aux_s);
     }
 
     return v;
 }
 
-fn write(arq: String, map_freq: &HashMap<char, u32>) -> io::Result<()> {
+fn write(arq: String, n: u32, map_freq: &HashMap<char, u32>, vec_bytes: Vec<String>) -> io::Result<()> {
+
+    println!();
+    println!("LEM MAP: {}", map_freq.len());
+    println!("N: {}", n);
+    println!();
+
     let mut fs = File::create_new(arq)?;
     let mut buffer = Cursor::new(Vec::new());
 
     buffer.write_u16::<BigEndian>(map_freq.len() as u16)?;
+    buffer.write_u32::<BigEndian>(n)?;
 
     for i in map_freq {
         buffer.write_u32::<BigEndian>(*i.0 as u32)?;
@@ -114,8 +129,21 @@ fn write(arq: String, map_freq: &HashMap<char, u32>) -> io::Result<()> {
     }
 
     fs.write_all(buffer.get_mut())?;
+
+    for b in vec_bytes{
+        match u8::from_str_radix(&b, 2) {
+            Ok(num) => {
+                buffer.write_u8(num)?;
+            },
+            Err(e) => {panic!("Não foi possível escrever o byte no arquivo, Error: {}", e)}
+        }
+    }
+
+    fs.write_all(buffer.get_mut())?;
+
     Ok(())
 }
+
 
 
 #[cfg(test)]
@@ -146,10 +174,11 @@ mod test {
     }
 
     #[test]
-    fn compression_test(){
+    fn compression_test() -> io::Result <()>{
         let mut map: HashMap<char, u32> = HashMap::new();
 
         let file = "test.txt";
+        let file_bin = "test_bin.bin";
 
         match frequency(file.to_string(), &mut map) {
             Ok(t) => {
@@ -159,7 +188,51 @@ mod test {
             
         }
 
-        compression(file.to_string());
+        compression(file.to_string(), file_bin.to_string())?;
+
+     
+        let num_char_test: u32 = 23;
+
+        // Le arquivo binário 
+        let mut fs = File::open(file_bin.to_string())?;
+        let mut v: Vec<u8> = Vec::new();
+        fs.read_to_end(&mut v)?;
+        let mut buffer = Cursor::new(v);
+
+        /*
+            Verifica se foi possui o mesmo número de caracteres distintos
+        */
+        let num_char_dist_test: u16 = map.len() as u16;
+        let num_char_dist: u16 = buffer.read_u16::<BigEndian>().unwrap();
+
+        println!("NUM CHAR DIST TEST: {}, NUM CHAR DIST: {}", num_char_dist_test, num_char_dist);
+        assert_eq!(num_char_dist_test, num_char_dist);
+
+        /*
+            Verifica se foi possui o mesmo número de caracteres
+        */
+        let num_char_test: u32 = 23;
+        let mum_char: u32 = buffer.read_u32::<BigEndian>().unwrap();
+
+        println!("NUM CHAR TEST: {}, NUM CHAR: {}", num_char_test, mum_char);
+        assert_eq!(num_char_test, mum_char);
+
+        /*
+            Verifica se o mapa de frequência lido no arquivo binário é igual ao 
+            mapa de frequência criado pelo mapa criado para o arquivo text
+        */
+        let mut c:char;
+        let mut f:u32;
+
+        for _ in 0..num_char_dist{
+            c = char::from_u32(buffer.read_u32::<BigEndian>().unwrap()).unwrap();
+            f = buffer.read_u32::<BigEndian>().unwrap();
+            println!("{c} - {f}");
+
+            assert_eq!(map.get(&c), Some(&f))
+        }
+
+        Ok(())
     }
 
     #[test]
@@ -171,7 +244,8 @@ mod test {
         assert_eq!("01".to_string(), v_s[0]);
         assert_eq!("01".to_string(), v_s[1]);
         assert_eq!("01".to_string(), v_s[2]);
-        assert_eq!("0".to_string(), v_s[3]);
+        assert_eq!("00000000".to_string(), v_s[3]);
     }
+
 }
 
